fceaad804dca79ad8b5f6388cfc3078ba616ec4a
Intishar Rahman Nafi <intisharrahmannafi@Intishars-Mac-mini.local>
2026-05-08 09:50:38 +0600

    add application and test configuration for Questr application

---
+++ src/main/java/com/app/questr/QuestrApplication.java
@@ -0,0 +1,13 @@
+package com.app.questr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/questr/QuestrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.questr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
